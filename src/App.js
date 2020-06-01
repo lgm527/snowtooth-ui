5d34ae627866ca1616875431cfbed6550107de1e
lgm527 <laurellmccaffrey@gmail.com>
2020-05-31 20:29:46 -0400

    user can change lift status

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery, useMutation } from "@apollo/client";
+import { StatusIndicator } from "./StatusIndicator";
 
 const QUERY = gql`
   query {
@@ -12,8 +13,19 @@ const QUERY = gql`
   }
 `;
 
+const MUTATION = gql`
+mutation ($status: LiftStatus! $id:ID!) {
+  setLiftStatus(status: $status id: $id) {
+    id
+    name
+    status
+  }
+}
+`;
+
 function App() {
   const { loading, data } = useQuery(QUERY);
+  const [setStatus] = useMutation(MUTATION);
   if (loading) return <p>loading lifts...</p>;
 
   return <section>
@@ -30,7 +42,18 @@ function App() {
           {data.allLifts.map(lift => (
             <tr>
               <td>{lift.name}</td>
-              <td>{lift.status}</td>
+              <td>
+                <StatusIndicator
+                status={lift.status}
+                onChange={status => setStatus({
+                  variables: {
+                    id: lift.id,
+                    status
+                  }
+                  })
+                }
+               />
+             </td>
             </tr>
           ))}
         </tbody>
