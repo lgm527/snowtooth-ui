182272d55b6c4252b4e05f2776e30f4ec1407f4a
lgm527 <laurellmccaffrey@gmail.com>
2020-05-31 20:09:18 -0400

    apollo connected to client

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://snowtooth.moonhighway.com/"
+  }),
+  cache: new InMemoryCache()
+});
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
