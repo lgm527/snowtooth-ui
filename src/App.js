bf8d5b290bad861865306ee1dca4ebe4e5845148
lgm527 <laurellmccaffrey@gmail.com>
2020-05-31 20:17:33 -0400

    all lift data handled

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import React from "react";
 import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const QUERY = gql`
+  query {
+    allLifts {
+      id
+      name
+      status
+    }
+  }
+`;
 
 function App() {
-  return <div>Build something cool!</div>;
+  const { loading, data } = useQuery(QUERY);
+  if (loading) return <p>loading lifts...</p>;
+
+  return <section>
+    <h1>Snowtooth Lift Status</h1>
+    {data && !loading && (
+      <table>
+        <thead>
+          <tr>
+            <th>Lift Name</th>
+            <th>Current Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allLifts.map(lift => (
+            <tr>
+              <td>{lift.name}</td>
+              <td>{lift.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )}
+  </section>;
 }
 
 export default App;
